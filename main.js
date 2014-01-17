14de557d95425a58fb6e719a49018f01b2e489a3
bitadj <bitadjaghouri@gmail.com>
2014-01-16 17:37:41 -0800

    rugged cell array tracking board--about to get crazy

---
+++ main.js
@@ -0,0 +1,22 @@
+
+// var turn = 0;
+
+
+// function init() {
+// 	var cells = document.getElementsByClassName("cell");
+// 	for (i in cells) {
+// 		cells[i].onclick = takeTurn;
+// 	}
+// }
+
+
+//  function takeTurn() {
+//  	if (this.innerHTML == "&nbsp;"){
+// 	 	if (turn%2 == 0) {
+// 	 		this.innerHTML = "x";
+// 	 	} else {
+// 	 		this.innerHTML = "o";
+// 	 	}
+// 	 	turn++;
+// 	 }
+//  }
