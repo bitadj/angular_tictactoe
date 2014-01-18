924c654e994ff087b4d3676ec7f325bbc6f43bbd
bitadj <bitadjaghouri@gmail.com>
2014-01-17 16:53:46 -0800

    add win conditions, logs win to console

--- main.js
+++ main.js
@@ -4,9 +4,44 @@ function ticTacCtrl ($scope) {
 	$scope.xTurn={val:false};
 
 	$scope.takeTurn = function(i, j) {
-		// angular.forEach($scope.cells)
-		$scope.cells[i][j] = ($scope.cells[i][j]=='' ? (($scope.xTurn.val = !$scope.xTurn.val) ? 'X' : 'O'): $scope.cells[i][j]);
+		$scope.cells[i][j] = ($scope.cells[i][j]=='' ? (($scope.xTurn.val = !$scope.xTurn.val) ? 'x' : 'o'): $scope.cells[i][j]);
+		$scope.checkWin()
 	}; //close takeTurn function
+
+	$scope.checkWin = function() {
+		for(var i=0; i<3; i++) {
+			var row = 0;
+			var col = 0;
+			var diag1 = 0;
+			var diag2 = 0;
+
+			for(var j=0; j<3; j++) {
+
+				if($scope.cells[i][j] == 'x') {row++};
+				if($scope.cells[j][i] == 'x') {col++};
+				if($scope.cells[j][j] == 'x') {diag1++};
+				if($scope.cells[j][2-j] == 'x') {diag2++};
+			}
+			if (row == 3 || col == 3 || diag1 == 3 || diag2 == 3) {
+					console.log('x WINS!')
+			}
+				// else {console.log('O WINS');}
+		};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	}; //close checkWin function
 
 	$scope.rightBorder = function(i,j) {
 		if ((i==0 && j==0) || (i==0 && j==1) || (i==1 && j==0) || (i==1 && j==1) || (i==2 && j==0) || (i==2 && j==1)){
